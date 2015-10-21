93df4193fc58c02cd52bca51f05769883692fa9d
Moises <47767573t@iespoblenou.org>
2015-10-21 20:22:26 +0200

    proyecto Eltemps 151021

--- app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
+++ app/src/main/java/com/example/poblenou/eltemps/MainActivityFragment.java
@@ -5,11 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> tiempoCadaDiaArray;
+    private ArrayAdapter tiempoCadaDiaAdapter;
+
+    private ListView tiempoCadaDia;
+
+    private View vRoot;
 
     public MainActivityFragment() {
     }
@@ -17,6 +28,30 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        vRoot = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String data [] = {
+                "Lun 23/6 - Soleado - 31/17",
+                "Mar 24/6 - Lluvioso - 31/17",
+                "Mie 25/6 - Soleado - 31/17",
+                "Jue 26/6 - Nieve - 31/17",
+                "Vie 27/6 - Nublado - 31/17",
+                "Sab 28/6 - Tormenta - 31/17",
+                "Sab 29/6 - Granizo - 31/17"
+        };
+
+
+        tiempoCadaDia = (ListView) vRoot.findViewById(R.id.lvTiempoCadaDia);
+
+        tiempoCadaDiaArray = new ArrayList<>();
+        tiempoCadaDiaAdapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_mainfragment,
+                R.id.textView1,
+                tiempoCadaDiaArray
+        );
+
+        return vRoot;
     }
 }
