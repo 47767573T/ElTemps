5bb7634e2720ca44ea0b57b79f0fcdbc1276ee6f
MVDam <47767573t@iespoblenou.org>
2015-10-25 23:26:01 +0100

    nuevos archivos y librerias para proyecto

---
+++ app/src/main/java/com/example/poblenou/eltemps/ElTempsFragment.java
@@ -0,0 +1,89 @@
+package com.example.poblenou.eltemps;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ElTempsFragment extends Fragment {
+
+    private ArrayList<String> tiempoCadaDiaArray = new ArrayList<>();
+    private ArrayAdapter tiempoCadaDiaAdapter;
+
+    private ListView lvElTempsMain;
+
+    private View vRoot;
+
+
+    Boolean menu = true;
+
+    public ElTempsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(menu);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        vRoot = inflater.inflate(R.layout.fragment_main, container, false);
+
+        /*String[] data = {
+                "Lun 23/6 - Soleado - 31/17",
+                "Mar 24/6 - Lluvioso - 31/17",
+                "Mie 25/6 - Soleado - 31/17",
+                "Jue 26/6 - Nieve - 31/17",
+                "Vie 27/6 - Nublado - 31/17",
+                "Sab 28/6 - Tormenta - 31/17",
+                "Sab 29/6 - Granizo - 31/17"
+        };*/
+
+
+        lvElTempsMain = (ListView)vRoot.findViewById(R.id.lvElTempsMain);
+
+        tiempoCadaDiaAdapter = new ArrayAdapter<>(
+                                getContext(),
+                                R.layout.lv_eltempsfragment,
+                                R.id.textView1,
+                                tiempoCadaDiaArray
+        );
+
+        return vRoot;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {return true;}
+
+        if (id == R.id.action_refresh) {
+            refresh();
+            return true;}
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh() {
+        OwmApiClient apiClient = new OwmApiClient();
+        apiClient.updateForecasts(tiempoCadaDiaAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/poblenou/eltemps/OwmApiClient.java
@@ -0,0 +1,102 @@
+package com.example.poblenou.eltemps;
+
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.example.poblenou.eltemps.json.Forecast;
+import com.example.poblenou.eltemps.json.List;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+
+/**
+* Created by Moises on 25/10/2015.
+*/
+
+interface OpenWeatherMapService {
+    @GET("forecast/daily")
+    Call<Forecast> dailyForecast(
+                                @Query("q") String city,
+                                @Query("mode") String format,
+                                @Query("units") String units,
+                                @Query("cnt") Integer num,
+                                @Query("appid") String appid);
+
+    //@POST()
+
+}
+
+public class OwmApiClient {
+    private OpenWeatherMapService owmService;
+
+    private final String forecastURL = "http://api.openweathermap.org/data/2.5/";
+    private final String forecastCITY = "Barcelona";
+    private final String forecastMODE = "json";
+    private final String forecastUNIT = "metric";
+    private final Integer forecastCNT = 1;
+    private final String forecastAPPID = "f50a8337632b64b69714d5b175384000";
+
+
+
+    public OwmApiClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl(forecastURL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+
+        owmService = retrofit.create(OpenWeatherMapService.class);
+    }
+
+    public void updateForecasts(final ArrayAdapter<String> adapter) {
+        Call<Forecast> forecastCall = owmService.dailyForecast(
+            forecastCITY, forecastMODE, forecastUNIT, forecastCNT, forecastAPPID
+        );
+
+        forecastCall.enqueue(new Callback<Forecast>() {
+            @Override
+            public void onResponse(Response<Forecast> response, Retrofit retrofit) {
+                Forecast forecast = response.body();
+
+                ArrayList<String> forecastStrings = new ArrayList<>();
+                for (List list : forecast.getList()) {
+                    String forecastString = getForecastString(list);
+                    forecastStrings.add(forecastString);
+
+                }
+                adapter.clear();
+                adapter.addAll(forecastStrings);
+            }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    Log.e("Update Forecasts", Arrays.toString(t.getStackTrace()));
+                }
+            });
+        }
+
+        private String getForecastString(List list) {
+        Long dt = list.getDt();
+        java.util.Date date = new java.util.Date(dt * 1000);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("E d/M");
+        String dateString = dateFormat.format(date);
+
+        String description = list.getWeather().get(0).getDescription();
+
+        Long min = Math.round(list.getTemp().getMin());
+        Long max = Math.round(list.getTemp().getMax());
+
+        return String.format("%s - %s - %s/%s", dateString, description, min, max
+        );
+    }
+}
